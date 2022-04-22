0e1234a4c71663bc41bdc8ed07722a8cf4057b4e
JonCabot <jfc0053@gmail.com>
2022-04-21 21:58:40 -0500

    commit tree 02f5255281f7d2e6d80bcc28bdea6d7fab186a85

--- db/migrate/20210719113216_create_games.rb
+++ db/migrate/20210719113216_create_games.rb
@@ -1,4 +1,4 @@
-class CreateGames < ActiveRecord::Migration[6.1]
+class CreateGames < ActiveRecord::Migration[6.1]    #sets up tables
   def change
     create_table :games do |t|
       t.string :title
